ac9791eb63b68967ced7bc30604b554dd9e983bd
rpatel121 <patelriyaz@yahoo.com>
2022-03-30 11:31:08 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
