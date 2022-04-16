e513841cc100e36da9e278f5b979e7a84089b97b
Riyaz Patel <patelriyaz@yahoo.com>
2022-04-16 04:22:05 +0000

    Variable & output code added

---
+++ output.tf
@@ -0,0 +1,10 @@
+
+output "Web-Server-URL" {
+  description = "Web-Server-URL"
+  value       = join("", ["http://", aws_instance.my-instance.public_ip])
+}
+
+output "Time-Date" {
+  description = "Date/Time of Execution"
+  value       = timestamp()
+}
